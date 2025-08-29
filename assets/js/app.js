b5e1c7e8af44669aac3e97c7856ec54f0a44f673
Shashith-Malshan <malshanshashith318@gmail.com>
2025-08-29 15:24:20 +0530

    Create Object Arrays

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+console.log("connected");
+
+let products = [
+  { name: "Classic Burger", price: 5, image: "assets/images/classic.jpg" },
+  { name: "Cheese Burger", price: 6, image: "assets/images/cheese.jpg" },
+  { name: "Veggie Burger", price: 4, image: "assets/images/veggie.jpg" },
+  { name: "French Fries", price: 3, image: "assets/images/frenchFries.jpg" },
+  { name: "Cola Drink", price: 2, image: "assets/images/cola.jpg" }
+];
+
+let order = [];
+let customers = [];
+let savedOrders = [];
+
+
